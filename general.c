04dd423b4d5ed65d0d91f7d99b392efbbc112a9f
Chris Binggeli <99316773+GNUNotUsername@users.noreply.github.com>
2023-02-03 14:54:03 +1000

    Style fixes, removed files which shouldn't have been committed

--- general.c
+++ general.c
@@ -16,56 +16,56 @@
 
 int
 init_connection(char *address, char **port, bool serverSide) {
-	struct addrinfo		*addrList, *check, criteria;
-	struct sockaddr_in	addr;
+    struct addrinfo     *addrList, *check, crits;
+    struct sockaddr_in  addr;
     char                *tport;
-	int					socketfd, err;
-	socklen_t			len;
+    int                 socketfd, err;
+    socklen_t           len;
 
-	/* Assume this is run by client and then rectify later if not */
-	socketfd = INVALID_SOCKET_FD;
-	bzero(&criteria, sizeof(struct addrinfo));
-	addrList = NULL;
-	criteria.ai_family = AF_UNSPEC;
-	criteria.ai_socktype = SOCK_STREAM;
-	err = 0;
+    /* Assume this is run by client and then rectify later if not */
+    socketfd = INVALID_SOCKET_FD;
+    bzero(&crits, sizeof(struct addrinfo));
+    addrList            = NULL;
+    crits.ai_family     = AF_UNSPEC;
+    crits.ai_socktype   = SOCK_STREAM;
+    err = 0;
 
-	if (serverSide) {
-		/* Alter a few options for server usage */
-		criteria.ai_flags = AI_PASSIVE;
-		address = NULL;
-	}
+    if (serverSide) {
+        /* Alter a few options for server usage */
+        crits.ai_flags  = AI_PASSIVE;
+        address         = NULL;
+    }
 
-	err = getaddrinfo(address, *port, &criteria, &addrList);
-	if (!err) {
-		for (check = addrList; check != NULL; check = check->ai_next) {
-			socketfd = socket(check->ai_family, check->ai_socktype,
-			    check->ai_protocol);
+    err = getaddrinfo(address, *port, &crit, &addrList);
+    if (!err) {
+        for (check = addrList; check != NULL; check = check->ai_next) {
+            socketfd = socket(check->ai_family, check->ai_socktype,
+                    check->ai_protocol);
 
-			if (socketfd == INVALID_SOCKET_FD) continue;
-			if (serverSide) {
-				err |= bind(socketfd,
-				    (struct sockaddr*)check->ai_addr,
-				    sizeof(struct sockaddr));
-				err |= listen(socketfd, INT_MAX);
-				if (!err) {
-					len = sizeof(struct sockaddr_in);
-					bzero(&addr, len);
-					err = getsockname(
-							socketfd, (struct sockaddr *)&addr, &len);
-				}
-			} else err = connect(socketfd,
-				(struct sockaddr*)check->ai_addr,
-				sizeof(struct sockaddr));
+            if (socketfd == INVALID_SOCKET_FD) continue;
+            if (serverSide) {
+                err |= bind(socketfd,
+                        (struct sockaddr*)check->ai_addr,
+                        sizeof(struct sockaddr));
+                err |= listen(socketfd, INT_MAX);
+                if (!err) {
+                    len = sizeof(struct sockaddr_in);
+                    bzero(&addr, len);
+                    err = getsockname(
+                            socketfd, (struct sockaddr *)&addr, &len);
+                }
+            } else err = connect(socketfd,
+                    (struct sockaddr*)check->ai_addr,
+                    sizeof(struct sockaddr));
 
-			if (!err) break;
-			close(socketfd);
-		}
-		freeaddrinfo(addrList);
-	}
+	        if (!err) break;
+            close(socketfd);
+        }
+        freeaddrinfo(addrList);
+    }
 
-	if (err)	socketfd = INVALID_SOCKET_FD;
-	else {
+    if (err) socketfd = INVALID_SOCKET_FD;
+    else {
         /* This could probably be better */
         free(*port);
         tport = SZALLOC(PORTSIZE);
@@ -73,5 +73,5 @@ init_connection(char *address, char **port, bool serverSide) {
         *port = tport;
     }
 
-	return (socketfd);
+    return (socketfd);
 }
--- server.c
+++ server.c
@@ -21,71 +21,70 @@
 /* Argv                     */
 #define ARGS_BEGIN          1
 #define ARGVINDRNG          3
-#define BAD_ARGC        	4
-#define PORT_DONE			128
-#define QUIET				"-q"
-#define QUIET_DONE			64
+#define BAD_ARGC            4
+#define PORT_DONE           128
+#define QUIET               "-q"
+#define QUIET_DONE          64
 
-/* Error Handling			*/
-#define DESTROY_ABORT(s)	if (s) free(s); s = NULL; goto ABORT
+/* Error Handling           */
+#define DESTROY_ABORT(s)    if (s) free(s); s = NULL; goto ABORT
 
-/* Error Returns			*/
-#define GOOD				0
-#define BAD_ARGS			1
+/* Error Returns            */
+#define GOOD                0
+#define BAD_ARGS            1
 
-/* IO						*/
-#define EOS					'\0'
-#define FREAD				"r"
-#define NEWLINE				'\n'
+/* IO                       */
+#define EOS                 '\0'
+#define FREAD               "r"
+#define NEWLINE             '\n'
 
-/* Networking				*/
-#define CURL_EXEC_FAIL		"Goddammit curl"
-#define GET_IP				"curl", "curl", "ifconfig.me", NULL
-#define INVALID_PORT		0
-#define RAND_PORT			6
+/* Networking               */
+#define CURL_EXEC_FAIL      "Goddammit curl"
+#define GET_IP              "curl", "curl", "ifconfig.me", NULL
+#define INVALID_PORT        0
+#define RAND_PORT           6
 
-/* Pipes					*/
-#define FINAL           	NULL, 0
-#define PIPE				2
-#define	READ				0
-#define WRITE				1
+/* Pipes                    */
+#define FINAL               NULL, 0
+#define PIPE                2
+#define	READ                0
+#define WRITE               1
 
-/* Status					*/
+/* Status                   */
 #define STARTLEN            42
 #define START_WITH_IP       "Server starting on %s:%s\n"
 #define START_NO_IP         "Server starting on port %s\n"
 #define SERVER_SOCKET_FAIL  "%s\n", "Could not create socket."
 
 
-void        accept_connections  (char *,         char*,  bool       );
-bool		is_numeric	        (char *								);
-int			asprintf	        (char **,	const char *,	...		);
+void        accept_connections  (char *,         char *,    bool    );
+bool        is_numeric	        (char *                             );
 
-char	*	machine_ip	        (void								);
-char	*	take_line	        (FILE *,	int *					);
-char	*	validate	        (int,		char **,		bool *  );
+char    *   machine_ip          (void                               );
+char    *   take_line	        (FILE *,    int *                   );
+char    *   validate            (int,       char **,        bool *  );
 
 int
 main(int argc, char **argv) {
-    char	*ip, *port;
-	bool	verbose;
+    char    *ip, *port;
+    bool    verbose;
 
     port = validate(argc, argv, &verbose);
-	if (!port) return BAD_ARGS;
-	ip = machine_ip();
+    if (!port) return BAD_ARGS;
+    ip = machine_ip();
 	
-	accept_connections(ip, port, verbose);
+    accept_connections(ip, port, verbose);
 
-	return (GOOD);
+    return (GOOD);
 }
 
 void
 accept_connections(char *addr, char *port, bool verbose) {
-	int     serverfd;
+    int     serverfd;
     char    *message, *tport;
 
     tport       = strdup(port);
-	serverfd    = init_connection(NULL, &tport, SERVERSIDE);
+    serverfd    = init_connection(NULL, &tport, SERVERSIDE);
     if (serverfd) {
         message = SZALLOC(STARTLEN);
         if (verbose) sprintf(message, START_WITH_IP, addr, tport);
@@ -100,94 +99,94 @@ accept_connections(char *addr, char *port, bool verbose) {
 
 bool
 is_numeric(char *test) {
-	bool	verdict;
-	int		check;
+	bool    verdict;
+    int     check;
 
-	check	= 0;
-	verdict	= true;
-	while ((test[check]) && (verdict = isdigit(test[check]))) check++;
+    check   = 0;
+    verdict = true;
+    while ((test[check]) && (verdict = isdigit(test[check]))) check++;
 
-	return (verdict);
+    return (verdict);
 }
 
 char *
 machine_ip(void) {
-	FILE	*child;
-	int		size, cStdout[PIPE];
-	pid_t	pid;
-	char	*ip;
+    FILE    *child;
+    int     size, cStdout[PIPE];
+    pid_t   pid;
+    char    *ip;
 
-	pipe(cStdout);
-	if ((pid = fork())) {
-		/* Parent	*/
-		close(cStdout[WRITE]);
-		waitpid(pid, FINAL);
-		child = fdopen(cStdout[READ], FREAD);
-		ip = take_line(child, &size);
-		fclose(child);
-	} else {
-		/* Child	*/
-		dup2(cStdout[WRITE], STDOUT_FILENO);
-		close(cStdout[READ]);
-		close(cStdout[WRITE]);
-		execlp(GET_IP);
-		ip = strdup(CURL_EXEC_FAIL);
-	}
+    pipe(cStdout);
+    if ((pid = fork())) {
+        /* Parent	*/
+        close(cStdout[WRITE]);
+        waitpid(pid, FINAL);
+        child = fdopen(cStdout[READ], FREAD);
+        ip = take_line(child, &size);
+        fclose(child);
+    } else {
+        /* Child	*/
+        dup2(cStdout[WRITE], STDOUT_FILENO);
+        close(cStdout[READ]);
+        close(cStdout[WRITE]);
+        execlp(GET_IP);
+        ip = strdup(CURL_EXEC_FAIL);
+    }
 
-	return (ip);
+    return (ip);
 }
 
 char *
 take_line(FILE *src, int *size) {
-	char	add, *out;
-	int		len;
+    char    add, *out;
+    int     len;
 
-	out = NULL;
-	len	= 0;
-	while (!(feof(src) || ferror(src))) {
-		add = fgetc(src);
-		if (add == EOF) break;
-		if (add == NEWLINE) add = EOS;
-		out = realloc(out, sizeof(char) * (len + 1));
-		out[len++] = add;
-	}
+    out = NULL;
+    len	= 0;
+    while (!(feof(src) || ferror(src))) {
+        add = fgetc(src);
+        if (add == EOF) break;
+        if (add == NEWLINE) add = EOS;
+        out = realloc(out, sizeof(char) * (len + 1));
+        out[len++] = add;
+    }
 
-	*size = len;
+    *size = len;
 
-	return (out);
+    return (out);
 }
 
 char *
 validate(int argc, char **argv, bool *verb) {
-	char		*cand, *port;
-	uint8_t		check;
+    char        *cand, *port;
+    uint8_t     check;
 
-	port	= NULL;
-	*verb	= true;
-	if (argc >= BAD_ARGC) return (port);
-	for (check = ARGS_BEGIN; (check & ARGVINDRNG) < argc; check++) {
-		cand = argv[(check & ARGVINDRNG)];
-		if (!strcmp(cand, QUIET)) {
-			if (check & QUIET_DONE) {
-				DESTROY_ABORT(port);
-			} else {
-				*verb = false;
-				check |= QUIET_DONE;
-			}
-		} else if (is_numeric(cand)) {
-			if (check & PORT_DONE) {
-				DESTROY_ABORT(port);
-			}
-			port	= strdup(cand);
-			check	|= PORT_DONE;
-		} else {
-			DESTROY_ABORT(port);
-		}
-	}
+    port    = NULL;
+    *verb   = true;
+    if (argc >= BAD_ARGC) return (port);
+    for (check = ARGS_BEGIN; (check & ARGVINDRNG) < argc; check++) {
+        cand = argv[(check & ARGVINDRNG)];
+        if (!strcmp(cand, QUIET)) {
+            if (check & QUIET_DONE) {
+                DESTROY_ABORT(port);
+            } else {
+                *verb = false;
+                check |= QUIET_DONE;
+            }
+        } else if (is_numeric(cand)) {
+            if (check & PORT_DONE) {
+                DESTROY_ABORT(port);
+            }
+            port    = strdup(cand);
+			check   |= PORT_DONE;
+        } else {
+            DESTROY_ABORT(port);
+        }
+    }
 
-	if (!(check & PORT_DONE)) port = strdup(EPHEMERAL);
+    if (!(check & PORT_DONE)) port = strdup(EPHEMERAL);
 
 ABORT:
 
-	return (port);
+    return (port);
 }
